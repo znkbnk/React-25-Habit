332d2bea863b0cb342f81b2190ab1c8f56c34de7
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-10-06 18:11:19 +0100

    Create CategoryDropdown.js

---
+++ CategoryDropdown.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+const CategoryDropdown = ({
+  showCategoriesDropdown,
+  onCategorySelect,
+  selectedCategory,
+  onCreateCategory,
+  categories,
+}) => {
+  const [newCategory, setNewCategory] = useState("");
+
+  const handleNewCategoryChange = (event) => {
+    setNewCategory(event.target.value);
+  };
+
+  const handleCreateCategory = () => {
+    if (newCategory.trim() !== "") {
+      onCreateCategory(newCategory);
+      setNewCategory("");
+    }
+  };
+
+  // Define your default categories here
+  const defaultCategories = ["All"];
+
+  return (
+    showCategoriesDropdown && (
+      <div className='dropdown-content'>
+        {defaultCategories.map((category, index) => (
+          <button
+            key={index}
+            onClick={() => onCategorySelect(category)}
+            className={`dropdown-link ${
+              category === selectedCategory ? "selected" : ""
+            }`}
+          >
+            {category}
+          </button>
+        ))}
+        {categories.map((category, index) => (
+          <button
+            key={index}
+            onClick={() => onCategorySelect(category)}
+            className={`dropdown-link ${
+              category === selectedCategory ? "selected" : ""
+            }`}
+          >
+            {category}
+          </button>
+        ))}
+        <div className='dropdown-link '>
+          <input
+            type='text'
+            placeholder='Create Category'
+            value={newCategory}
+            onChange={handleNewCategoryChange}
+            className='category-input'
+          />
+          {newCategory.trim() !== "" && (
+            <button onClick={handleCreateCategory} className='category-button'>
+              Create
+            </button>
+          )}
+        </div>
+      </div>
+    )
+  );
+};
+
+export default CategoryDropdown;
